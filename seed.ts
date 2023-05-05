c0cf266c32c63764bf8702cdc6800909be36e986
santrami <santrami@hotmail.com>
2023-05-05 21:45:14 +0200

    adding delete endpoint

--- seed.ts
+++ seed.ts
@@ -1,32 +1,39 @@
-import express from 'express';
-import {PrismaClient} from '@prisma/client'
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app= express();
-
-app.listen(3000, ()=>{
-    console.log('listening on port 3000');
-    
-});
+const app = express();
 const prisma = new PrismaClient();
 
-app.use(express.json())
-/* app.use(express.urlencoded({ extended: true }))
- */
-app.get('/', (req, res) => {
-    res.send('hola mundo');
-})
-
-app.post('/post', async(req, res) => {
-    const {title, year, duration}= req.body;
-    const result = await prisma.movie.create({
-        data: {
-            title, year, duration
-        }
-    });
-    res.json(result)
+app.listen(3000, () => {
+  console.log("listening on port 3000");
 });
 
-console.log('listening on port 3000');
+app.use(express.json());
+/* app.use(express.urlencoded({ extended: true }))
+ */
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+
+app.post("/post", async (req, res) => {
+  const { title, year, duration } = req.body;
+  const result = await prisma.movie.create({
+    data: {
+      title,
+      year,
+      duration,
+    },
+  });
+  res.json(result);
+});
+
+app.delete('/post/:id', async (req, res) => {
+    const {id}= req.params;
+    const post = await prisma.movie.delete({
+        where: {id:Number (id)}
+    });
+    res.json('eliminado');
+})
 
 /* 
 const newMovie = await prisma.movie.create({
